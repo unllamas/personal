3dfabdd8d0ece0ee0a153da15ef7f00fbe3ab2e5
Jona <llamasjonat@gmail.com>
2022-12-29 15:13:55 +0100

    feat: integrated Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,36 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import Script from 'next/script';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import theme from '../theme';
+import { AppProps } from 'next/app';
+
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
