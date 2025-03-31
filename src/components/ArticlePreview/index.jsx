253a6c676a13eafe039b45248de88f5244235c1d
Jona <llamasjonat@gmail.com>
2025-03-31 11:57:28 -0300

    fix(component): cleaned ArticlePreview

--- src/components/ArticlePreview/index.jsx
+++ src/components/ArticlePreview/index.jsx
@@ -7,7 +7,7 @@ export default function ArticlePreview({ article }) {
   const { image, url, title } = article;
 
   return (
-    <LinkBox as='article' position='relative' role='group'>
+    <Link href={url?.href} target={url?.target} tabIndex={-1}>
       <VStack gap='10px'>
         <Box
           backgroundImage={`/articles/${image.url}`}
@@ -23,31 +23,17 @@ export default function ArticlePreview({ article }) {
           overflow='hidden'
         />
         <HStack alignItems='flex-start' justifyContent='space-between' px='10px' w='100%' gap='20px'>
-          <Heading as='h3' fontSize='22px' _groupHover={{ textDecoration: 'underline' }}>
-            <Link href={url?.href} target={url?.target} tabIndex={-1}>
-              {title}
-            </Link>
+          <Heading
+            as='h3'
+            fontSize='20px'
+            textUnderlineOffset={4}
+            textDecoration='underline'
+            textDecorationColor='gray.300'
+          >
+            {title}
           </Heading>
-
-          <Box>
-            <LinkOverlay
-              href={url?.href}
-              target={url?.target}
-              tabIndex={0}
-              display='flex'
-              justifyContent='center'
-              alignItems='center'
-              minH='50px'
-              minW='50px'
-              bg='#fff'
-              borderRadius='12px'
-              border='1px solid #eee'
-            >
-              <Arrow />
-            </LinkOverlay>
-          </Box>
         </HStack>
       </VStack>
-    </LinkBox>
+    </Link>
   );
 }
